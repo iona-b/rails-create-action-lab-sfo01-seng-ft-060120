4782488d245843eb09597c3f1911aa3b2143ae1b
iona-b <ionabrabender@gmail.com>
2020-06-23 15:38:22 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.new(first_name: "Iona", last_name: "Brabender")
